546170a2ec39c14dedb231f85ae59d694196f42c
Raphael Rocha <rocharaphael0911@outlook.com>
2024-02-06 14:59:09 -0300

    Fazendo loop para adicionar o audio aos botoes

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
 
-function pom() {
-    document.querySelector('#som_tecla_pom').play()
+function tocaSom(idElementoAudio) {
+    document.querySelector(`#som_${idElementoAudio}`).play()
 }
 
 
-document.querySelector('.tecla_pom').onclick = pom
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+listaDeTeclas.forEach(tecla => {
+    tecla.addEventListener('click', () => tocaSom(tecla.classList[1]))
+})
+
+// var i = 0;
+// while (i < listaDeTeclas.length) {
+//     const tecla = listaDeTeclas[i]
+//     tecla.onclick = () => tocaSom(tecla.classList[1])
+//     i++;
+// }
