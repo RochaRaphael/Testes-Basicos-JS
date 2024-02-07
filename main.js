223d941143f6d6288d1737e608f0528bfadceca7
Raphael Rocha <rocharaphael0911@outlook.com>
2024-02-07 12:10:53 -0300

    Adicionando regras ao apertar deperminadas teclas

--- main.js
+++ main.js
@@ -8,6 +8,16 @@ const listaDeTeclas = document.querySelectorAll('.tecla');
 
 listaDeTeclas.forEach(tecla => {
     tecla.addEventListener('click', () => tocaSom(tecla.classList[1]))
+
+    tecla.onkeydown = function (event) {
+        if (event.code == 'Space' || event.code == 'Enter') {
+            tecla.classList.add('ativa');
+        }
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
 })
 
 // var i = 0;
@@ -16,3 +26,4 @@ listaDeTeclas.forEach(tecla => {
 //     tecla.onclick = () => tocaSom(tecla.classList[1])
 //     i++;
 // }
+
